63495c83e36728f09ed820f208c749f375466745
SIVANESWARAN JUNOJ <junojsiva@gmail.com>
2024-07-30 10:43:08 +0530

    GradeMaster design

---
+++ New SMS UI/UI/Grade/GradeMaster.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace New_SMS_UI.UI.Grade
+{
+    partial class GradeMaster
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
+            groupBox1 = new GroupBox();
+            dgvGrade = new DataGridView();
+            groupBox2 = new GroupBox();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            guna2GradientPanel1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvGrade).BeginInit();
+            SuspendLayout();
+            // 
+            // guna2GradientPanel1
+            // 
+            guna2GradientPanel1.BackColor = Color.Transparent;
+            guna2GradientPanel1.Controls.Add(groupBox1);
+            guna2GradientPanel1.CustomizableEdges = customizableEdges3;
+            guna2GradientPanel1.Dock = DockStyle.Fill;
+            guna2GradientPanel1.FillColor = Color.FromArgb(96, 96, 96);
+            guna2GradientPanel1.FillColor2 = Color.Black;
+            guna2GradientPanel1.Location = new Point(0, 0);
+            guna2GradientPanel1.Name = "guna2GradientPanel1";
+            guna2GradientPanel1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2GradientPanel1.Size = new Size(1053, 531);
+            guna2GradientPanel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(guna2Button1);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Controls.Add(dgvGrade);
+            groupBox1.Font = new Font("Times New Roman", 13.2000008F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = SystemColors.ControlLightLight;
+            groupBox1.Location = new Point(23, 8);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1004, 504);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Grade Master";
+            // 
+            // dgvGrade
+            // 
+            dgvGrade.BackgroundColor = SystemColors.ControlLightLight;
+            dgvGrade.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvGrade.Location = new Point(10, 94);
+            dgvGrade.Name = "dgvGrade";
+            dgvGrade.RowHeadersWidth = 51;
+            dgvGrade.Size = new Size(586, 294);
+            dgvGrade.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox2.ForeColor = SystemColors.ControlLightLight;
+            groupBox2.Location = new Point(630, 81);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(346, 307);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Grade Details";
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.BorderColor = Color.White;
+            guna2Button1.BorderRadius = 15;
+            guna2Button1.BorderThickness = 3;
+            guna2Button1.CustomizableEdges = customizableEdges1;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            guna2Button1.FillColor = Color.Transparent;
+            guna2Button1.Font = new Font("Segoe UI", 9F);
+            guna2Button1.ForeColor = Color.White;
+            guna2Button1.Location = new Point(396, 427);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Button1.Size = new Size(107, 56);
+            guna2Button1.TabIndex = 2;
+            guna2Button1.Text = "guna2Button1";
+            // 
+            // GradeMaster
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1053, 531);
+            Controls.Add(guna2GradientPanel1);
+            Name = "GradeMaster";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "GradeMaster";
+            guna2GradientPanel1.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvGrade).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private DataGridView dgvGrade;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+    }
+}
